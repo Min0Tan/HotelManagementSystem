3089333dee6c64de11526e89e26890ead5f206e0
Min0Tan <bobcat0255@gmail.com>
2022-03-28 11:14:48 +0900

    Menu added

--- src/HotelManagementSystem/HotelManagementSystem.java
+++ src/HotelManagementSystem/HotelManagementSystem.java
@@ -1,3 +1,5 @@
+package HotelManagementSystem;
+
 import java.util.Scanner;
 
 public class HotelManagementSystem {
@@ -10,11 +12,13 @@ public class HotelManagementSystem {
 				System.out.println("1. Select Room to use");
 				System.out.println("2. Delete Room");
 				System.out.println("3. Edit Room");
-				System.out.println("4. View Rooms");
+				System.out.println("4. View Room");
 				System.out.println("5. Exit");
 				System.out.println("Select one number 1~5");
 				num = input.nextInt();
+				
 				switch (num) {
+				
 				case 1:
 					System.out.println("Input your room number that you want to use: ");
 					String room = input.next();
@@ -27,16 +31,38 @@ public class HotelManagementSystem {
 						}
 					}
 					break;
+					
 				case 2:
-					System.out.println("Select Room to Delete");
+					deleteRoom();
 					break;
+					
 				case 3:
-					System.out.println("Select Room to Edit");
+					editRoom();
 					break;
+					
 				case 4:
-					System.out.println("View Rooms");
+					viewRooms();
 					break;
 				}
 			}
 		}
+		
+		public static void deleteRoom() {
+				Scanner input = new Scanner(System.in);
+				System.out.print("Enter the Room number to Delete: ");
+				int delroom = input.nextInt();
+		}
+		
+		public static void editRoom() {
+			Scanner input = new Scanner(System.in);
+			System.out.print("Enter the Room number to Edit: ");
+			int editroom = input.nextInt();
+		}
+		
+		public static void viewRooms() {
+			Scanner input = new Scanner(System.in);
+			System.out.print("Enter the Room number to View: ");
+			int viewroom = input.nextInt();
+		}
+		
 }
